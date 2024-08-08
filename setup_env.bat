69cb7be19fb7c86b10cb51f007d09546704e835d
FujimotoShota-toruca <fujimoto.shota.0401.0304@gmail.com>
2024-08-09 03:22:58 +0900

    setup_env.batのPythonやpipのバージョンの指定

--- setup_env.bat
+++ setup_env.bat
@@ -1,9 +1,12 @@
 @echo off
-REM 仮想環境の作成
-python -m venv my_env
+REM 仮想環境の作成（Python 3.11を使用）
+python3.11 -m venv my_env
 
 REM 仮想環境の有効化
 call my_env\Scripts\activate
+
+REM pipを指定バージョンにアップグレード
+python -m pip install --upgrade pip==24.2
 
 REM 必要なパッケージをインストール（例としてrequirements.txtを使う）
 if exist requirements.txt (
